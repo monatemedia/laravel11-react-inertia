22f3a694742c4844269c05d18425c22b9e612bbd
Edward Baitsewe <edward@monatemedia.com>
2024-03-27 01:58:40 +0200

    Prepare the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Project;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +17,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Edward',
+            'email' => 'edward@gmail.com',
+            'password' => bcrypt('12345678'),
+            'email_verified_at' => time()
         ]);
+
+        Project::factory()
+            ->count(30)
+            ->hasTasks(30)
+            ->create();
     }
 }
