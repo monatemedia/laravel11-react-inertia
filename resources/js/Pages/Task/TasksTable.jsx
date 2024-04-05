b7c85ae0f11a747b654ca26ff8016e4c92a43354
Edward Baitsewe <edward@monatemedia.com>
2024-04-06 01:12:48 +0200

    Create MyTasks page

--- resources/js/Pages/Task/TasksTable.jsx
+++ resources/js/Pages/Task/TasksTable.jsx
@@ -152,8 +152,10 @@ export default function TasksTable({
                 {!hideProjectColumn && (
                   <td className="px-3 py-2">{task.project.name}</td>
                 )}
-                <td className="px-3 py-2">{task.name}</td>
-                <td className="px-3 py-2">
+                <th className="px-3 py-2 hover:underline text-gray-100">
+                  <Link href={route("task.show", task.id)}>{task.name}</Link>
+                </th>
+                <td className="px-3 py-2 text-nowrap text-white">
                   <span
                     className={
                       "px-2 py-1 rounded text-white " +
